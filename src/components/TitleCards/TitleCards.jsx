94cb0d76c0804e153d376cedb4b0b18e64ed2f71
Scott Slagle <spsbiz@hotmail.com>
2026-02-03 17:35:32 -0800

    remove local cards, use TMDB image only

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
-import cards_data from "../../assets/cards/Cards_data.js";
 import { Link } from "react-router-dom";
 
 const TitleCard = ({ title, category }) => {
